9019afbb981c5506789741c463f303a906e0e7dd
softnix <suriya.jaboon242@gmail.com>
2018-10-01 14:00:12 +0700

    Change Strucutre

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import http from 'http'
-import routes from './routes/index'
+import routes from '@/routes/index'
+
 const app = express()
 const port = process.env.PORT || 3030
 app.use('/node', routes)
